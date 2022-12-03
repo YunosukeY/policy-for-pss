acb4ccfd627204030487802007c3c001b326fbea
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2022-12-04 00:52:09 +0900

    ci: fix (#17)
    * chore: install aqua
    
    * ci: update opa test
    
    * docs: update
    
    * ci: add conftest
    
    * chore: upload artifact
    
    * ci: check template is up-to-date
    
    * ci: e2e
    
    * ci: use cmd via sh

---
+++ e2e/e2e.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+repo_dir="$(git rev-parse --show-toplevel)"
+
+kind create cluster
+helmfile apply -f "${repo_dir}/e2e"
+kubectl apply -f "${repo_dir}/k8s/template_Policy.yaml"
+sleep 1 # hack
+kubectl apply -f "${repo_dir}/k8s/constraint_Policy.yaml"
+kubectl create namespace test
+
+kubectl apply -f "${repo_dir}/example/safe.yaml"
+! kubectl apply -f "${repo_dir}/example/unsafe.yaml"
+kubectl apply -f "${repo_dir}/example/allowed.yaml"
