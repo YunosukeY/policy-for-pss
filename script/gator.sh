c50cf29b9cc3682ad91e2fedace2849e6a9fee53
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2023-11-19 09:39:47 +0900

    readonly

--- script/gator.sh
+++ script/gator.sh
@@ -2,21 +2,21 @@
 
 set -eu
 
-version="v3.14.0"
-repo_dir="$(git rev-parse --show-toplevel)"
-bin="${repo_dir}/bin"
-gator="${bin}/gator"
+readonly VERSION="v3.14.0"
+readonly REPO_DIR="$(git rev-parse --show-toplevel)"
+readonly BIN="${REPO_DIR}/BIN"
+readonly GATOR="${BIN}/gator"
 
-if [[ -x "${gator}" ]]; then
+if [[ -x "${GATOR}" ]]; then
   true
 else
-  mkdir -p "${bin}"
-  echo "download gator ${version}"
-  url="https://github.com/open-policy-agent/gatekeeper/releases/download/${version}/gator-${version}-linux-amd64.tar.gz"
+  mkdir -p "${BIN}"
+  echo "download gator ${VERSION}"
+  url="https://github.com/open-policy-agent/gatekeeper/releases/download/${VERSION}/gator-${VERSION}-linux-amd64.tar.gz"
   curl -sfSLO "$url"
-  tar -zxvf "gator-${version}-linux-amd64.tar.gz"
-  mv gator "${bin}"
-  chmod +x "${gator}"
+  tar -zxvf "gator-${VERSION}-linux-amd64.tar.gz"
+  mv gator "${BIN}"
+  chmod +x "${GATOR}"
 fi
 
-"${gator}" "${@}"
+"${GATOR}" "${@}"
